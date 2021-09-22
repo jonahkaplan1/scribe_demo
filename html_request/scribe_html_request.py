a269ab00d42bf35eaab5a6bb851c25ebf987e2d6
Jonah Kaplan <kaplanjonah95@gmail.com>
2021-09-22 16:59:17 -0700

    html_request add comments

--- html_request/scribe_html_request.py
+++ html_request/scribe_html_request.py
@@ -11,20 +11,18 @@ body = {
     "sql_contents": sql_file,
     "scribe_api_key": 'my_api_key', # Replace this with your API Key
     "tenancy": "prod",
-    "produce_scribe_json_response": False,
-    "produce_html_documentation": True
+    "produce_scribe_json_response": False, # In this example we dont want the json response
+    "produce_html_documentation": True # We DO want the html string response
 }
 
 # Make our request
 response = requests.post('https://scribedata.app/scribe', json=body)
 response_json = response.json()
 
-print(response_json)
-
 
 # The html documentation response object is a string of raw html.
 # It can be written directly to an HMTL File
 file_path = "Downloads/my_query_visualized.html"
-text_file = open(file_path, "w")
-n = text_file.write(response_json['html_documentation'])
+text_file = open(file_path, "w") # Create the file in the path above
+n = text_file.write(response_json['html_documentation']) # Write out HTML string directly to the new file
 text_file.close()
